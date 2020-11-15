7baa58e7003c332c26b0a7cd1094234f9c7a1a66
Anas El Baghdadi <60518910+ael-bagh@users.noreply.github.com>
2020-11-15 20:55:26 +0100

    Update map_manager.c

--- map_manager.c
+++ map_manager.c
@@ -6,20 +6,58 @@
 /*   By: ael-bagh <ael-bagh@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/11/14 11:46:48 by ael-bagh          #+#    #+#             */
-/*   Updated: 2020/11/14 11:55:57 by ael-bagh         ###   ########.fr       */
+/*   Updated: 2020/11/15 20:53:07 by ael-bagh         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
 
-int longuest_line()
+int     longuest_line(void)
 {
-        
+    int i;
+    int j;
+    int max;
+
+    i = 0;
+    j = 0;
+    max = 0;
+        while(g_map_array[i])
+        {
+            while(g_map_array[i][j] != '\0')
+                j++;
+            i++;
+            if (j > max)
+                max = j;
+            j = 0;
+        }
+        //printf("longuest line:%d\n", max);
+        return (max);
 }
 
-void map_manager(void)
+int     array_height(void)
+{
+        int i;
+
+        i = 0;
+        while (g_map_array[i])
+           i++; 
+        return (i);
+}    
+
+char    **map_manager(void)
 {
     char **map_array;
+    int i;
 
-    
+    i = 0;
+
+    map_array = (char**)malloc(sizeof(char*) * array_height());
+    while (i < longuest_line())
+    {
+        map_array[i] = (char*)malloc(sizeof(char) * longuest_line());
+        map_array[i] = g_map_array[i];
+        i++;
+    }
+    freethenipples(map_array);
+    return (map_array);
 }
